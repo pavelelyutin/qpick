a0a1c6e2ac0152a67986cd934a01525a2527ba45
pavelelyutin <elyutin.pavel@gmail.com>
2026-09-18 16:27:21 +0300

    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import './App.scss'
+import Header from './components/Header'
 
 function App() {
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            <Header />
+        </>
+    )
 }
 
 export default App
