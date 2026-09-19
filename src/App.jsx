f2f662da3d9ce1abb6e17ede5c6539e2c08a499c
Pavel Elyutin <elyutin.pavel@gmail.com>
2026-09-19 16:21:33 +0300

    add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.scss'
-import Header from './components/Header'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
-    return (
-        <>
-            <Header />
-        </>
-    )
+  return (
+    <>
+      <Header/>
+      <main>
+
+      </main>
+      <Footer/>
+    </>
+  )
 }
 
 export default App
